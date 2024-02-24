ecb6053804743b4c390fec27b3dcde7ccc915269
sana.hsu <sana6622@gmail.com>
2024-02-25 00:38:03 +0800

    create

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# vue-week6
+# fun-drink
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base:"vue-week6/",
+  base:"fun-drink/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
